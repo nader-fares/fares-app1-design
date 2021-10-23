9df276ede47ccc5b52d46e1c2ff0e692a7d71ffe
nader-fares <59976904+nader-fares@users.noreply.github.com>
2021-10-22 23:37:30 -0400

    initial GUI design

--- ToDoApp/src/main/java/baseline/Application.java
+++ ToDoApp/src/main/java/baseline/Application.java
@@ -1,7 +1,25 @@
 package baseline;
 
-public class Application {
-    public static void main(String[] args) {
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+
+
+public class Application extends javafx.application.Application {
+
+    public static void main(String[] args) {
+        launch(args);
     }
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
