048f3979db7ceac88e71477fa6c4c2d0ba29c962
nader-fares <59976904+nader-fares@users.noreply.github.com>
2021-10-24 23:23:47 -0400

    updated gui

--- ToDoApp/src/main/java/baseline/Application.java
+++ ToDoApp/src/main/java/baseline/Application.java
@@ -1,25 +1,29 @@
+/*
+ *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Nader Fares
+ */
 package baseline;
 
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+import javafx.fxml.FXMLLoader;
 
 
 public class Application extends javafx.application.Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Application.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("JavaFX and Gradle");
         stage.setScene(scene);
         stage.show();
     }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 
 }
---
+++ ToDoApp/src/main/java/baseline/Controller.java
@@ -0,0 +1,40 @@
+package baseline;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void addList(ActionEvent actionEvent) {
+    }
+
+    public void delList(ActionEvent actionEvent) {
+    }
+
+    public void editList(ActionEvent actionEvent) {
+    }
+
+    public void addItem(ActionEvent actionEvent) {
+    }
+
+    public void editItem(ActionEvent actionEvent) {
+    }
+
+    public void showAllItems(ActionEvent actionEvent) {
+    }
+
+    public void showIncompleteItems(ActionEvent actionEvent) {
+    }
+
+    public void showCompleteItems(ActionEvent actionEvent) {
+    }
+
+    public void saveTodoList(ActionEvent actionEvent) {
+    }
+
+    public void loadTodoList(ActionEvent actionEvent) {
+    }
+
+    public void markItemComplete(ActionEvent actionEvent) {
+    }
+}
+
+
